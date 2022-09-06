e48b7a06077616d77d8bf8424006fb84caa37dc8
Sophia Han <eunjookim@sophia-macbook.myfiosgateway.com>
2022-09-06 14:00:14 -0400

    First Commit

---
+++ SECp_04_Variables and Data Types.js
@@ -0,0 +1,41 @@
+alert("Hello from JS!");
+
+// 1. Create the following variables
+// name, which is a string set to your current name
+// dayOfBirth, which is a number set to the day of your birth month
+
+
+let dayOfBirth = 21;
+let name = "Sophia Han";
+
+// 2. See what happens when you have multiple variables of the same name. Which one takes precedence?
+
+let name = "Dean"
+name;
+
+// name  = "Dean" (The second one)
+
+// 3. Write some JavaScript that prompt’s the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+
+let favoriteColor = prompt("what is your favorite color?");
+console.log("Your favorite color is " + favoriteColor);
+
+// 4. Create a string that contains both single quotes and double quotes.
+
+let quotes = "I'm Sophia Han."
+
+// 5. What is the difference between null and undefined?
+
+// null is for the purpose of explicitly defining "no" value,
+// whereas undefined is the default value for variables that are not assigned.
+
+// 6. What is NaN in JavaScript? What is the typeof NaN?
+
+// NaN is a value JavaScript returns when trying to do a numerical operation
+// on something that is not a number. typeof NaN is "number"
+
+// 7. You can declare a variable by typing let thing;. What is the value of thing?
+
+// undefined
+
+
